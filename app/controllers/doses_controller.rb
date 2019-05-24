b984c84a7adecd64e836aeb431da3659c1d195b7
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-05-24 11:53:10 +0200

    added heroku

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else 
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path @dose.cocktail
+  end
+
+  private 
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
+
